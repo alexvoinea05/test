4ff1e88f9571e05ff64e92103a4453e236a9dc2b
unknown <alexandru.voinea5@s.unibuc.ro>
2022-10-13 13:22:51 +0300

    add auth

---
+++ src/main/java/com/auth/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.auth.repository;
+
+import com.auth.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
